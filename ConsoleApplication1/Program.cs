289c925a5340a37ea5637e348d0501129a31e583
Richard Beacroft <richard.beacroft@forfront.com>
2017-06-26 11:52:49 +0100

    Updated

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication1
+﻿namespace ConsoleApplication1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            // This is a small change
+
+            //branched
         }
     }
 }
