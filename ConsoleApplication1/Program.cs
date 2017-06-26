660f3e4139b8a37b6666222b98a5d9cd5ac28ae5
Richard Beacroft <richard.beacroft@forfront.com>
2017-06-26 12:25:22 +0100

    Updated with instructions

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,9 +4,19 @@
     {
         private static void Main(string[] args)
         {
-            // This is a small change
-
-            //branched
+            /*
+             * Actions taken to link VS Git to GitHub repository
+             * Install Git from https://git-scm.com/download/win and used all defaults
+             * Installed Visual Studio - Git Source Control Provider
+             * Tools\Options, set the Source Control plugin to Git Source Control Provider
+             * Installed Visual Studio - Git Extensions (provider right-click menu for all Git operations)
+             * From the command line ran; puttygen.exe, clicked generate and saved ppk file with sensible name to sensible place
+             * Logged into GitHub, went to Settings/SSH and GPG Keys, clicked New SSH Key and pasted in puttygen generated key
+             * From Visual Studio, right-clicked on the solution and selected "Manage Remotes", entered the git URL to the repository and browsed to the ppk file saved earlier
+             * 
+             * Testing: 
+             * Open project in visual studio you wish to push or push changes to GitHub. Right click, select "Git(<branch>)/Push" and select the remote repository and click Push
+             */
         }
     }
 }
